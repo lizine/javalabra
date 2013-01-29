31b09fea107584cd7fe616dfb89d6bb14353086e
Nina A L Liljeblad <lini@melkki.cs.helsinki.fi>
2013-01-29 16:39:33 +0200

    nappikuuntelija-luokan lisays

---
+++ Yatzy/src/GUI/Kuuntelija.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/**
+ *
+ * @author lini
+ */
+public class Kuuntelija implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
